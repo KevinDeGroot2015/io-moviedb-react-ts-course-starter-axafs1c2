2f718d50496b4dbf3de69158e0fdcb5b3ac2f4c7
Kevin de Groot <kevin.van.der.bijl@divide.nl>
2025-04-23 09:31:15 +0200

    Router uitbreiden, overige pagina's koppelen o.b.v. specs

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,12 +2,18 @@ import { FunctionComponent } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Layout from './Layout';
 import Home from './Home';
+import Detail from './Detail';
+import Favorites from './Favorites';
+import Edit from './Edit';
 
 const Router: FunctionComponent = ({}) => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />}/>
+        <Route path="/:id" element={<Detail />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/favorites/:id" element={<Edit />} />
       </Route>
     </Routes>
   );
