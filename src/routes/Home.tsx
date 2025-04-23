951a56c3fa42e45a39bc6736c205e942618e7e04
Kevin de Groot <kevin.van.der.bijl@divide.nl>
2025-04-23 10:36:29 +0200

    Basis opzet data verkrijgen op de Home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,26 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
+import { IMDBMovie } from '../model/movie';
 
 const Home: FunctionComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.omdbapi.com/?apikey=1a993ee0&s=blade')
+    .then((r) => r.json())
+    .then((d) => setData(d.Search))
+
+    console.log(data)
+  },[])
+
   return (
-    <>
-      <h1>Home test</h1>
       <ul
         role="list"
         className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-      ></ul>
-    </>
+      >
+        {data?.map((item: IMDBMovie) => {
+          return <li>{item.Title}</li>
+        })}
+      </ul>
   );
 };
 
