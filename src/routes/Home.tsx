5590207608b3005c7f17ba9493b1e1480b154ab5
Kevin de Groot <kevin.van.der.bijl@divide.nl>
2025-04-23 12:09:41 +0200

    Overzichtlijst wat mooier maken, ontbrekende items plaatsen

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,6 +5,7 @@ import { IMDBMovie } from '../model/movie';
 
 const Home: FunctionComponent = () => {
   const { data } = useContext(DataContext);
+  console.log(data)
 
   return (
       <ul
@@ -12,9 +13,20 @@ const Home: FunctionComponent = () => {
         className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
       >
         {data && data.length > 0 ? (
-          data.map((movie: IMDBMovie) => {
-            return <li key={movie.imdbID}>{movie.Title}</li>;
-          })
+          data.map((movie: IMDBMovie) => (
+            <li
+              key={movie.imdbID}
+              className="bg-white rounded-2xl text-center shadow-md hover:shadow-xl transition-shadow duration-300 overflow-hidden"
+            >
+              <img
+                src={movie.Poster}
+                alt={movie.Title}
+                className="w-full h-72 object-cover"
+              />
+              <p className="p-4 font-bold text-lg text-gray-800">{movie.Title}</p>
+              <p className="pb-4">{movie.Year}</p>
+            </li>
+          ))
         ) : (
           <li>Search all movies!</li>
         )}
