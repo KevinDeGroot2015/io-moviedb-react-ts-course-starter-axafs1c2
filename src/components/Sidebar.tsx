ad1b148fad67813c0182cdb3529864ce8619d199
Kevin de Groot <kevin.van.der.bijl@divide.nl>
2025-04-23 09:51:15 +0200

    Sidebar links toevoegen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar: FunctionComponent = ({}) => {
   return (
@@ -6,7 +7,20 @@ const Sidebar: FunctionComponent = ({}) => {
       aria-label="Sidebar"
       className="sticky top-4 divide-y divide-gray-300"
     >
-      <div className="pb-8 space-y-1">{/* navigation */}</div>
+      <div className="pb-8 space-y-2">
+        <Link
+          to="/"
+          className="block text-center bg-gray-200 py-2 px-3 shadow hover:bg-gray-500 hover:text-white transition"
+        >
+          Home
+        </Link>
+        <Link
+          to="/favorites"
+          className="block text-center bg-gray-200 py-2 px-3 shadow hover:bg-gray-500 hover:text-white transition"
+        >
+          Favorites
+        </Link>
+      </div>
     </nav>
   );
 };
